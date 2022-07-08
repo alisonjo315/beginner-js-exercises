f8498884ca2d9e3aed8b581d47c094c1ee7c9b6e
Alison <alison@cornell.edu>
2022-07-08 15:39:26 -0400

    functions notes and stuff

--- playground/types_functions.js
+++ playground/types_functions.js
@@ -9,7 +9,6 @@ let age3 = 100;
 
 // blah blah blah watched video...
 
-// some funtions return things to you, some just do work and don't return anything
+// some funtions return things to you, some just do stuff and don't return anything
 console.log('10'); // does a thing, doesn't return anything
-
 Date.now(); // returns unix time in milliseconds
