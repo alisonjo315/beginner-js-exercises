fb2d434bd1eaaae361281f16712eb9e29fc7b54d
Alison <alison@cornell.edu>
2022-07-08 15:22:43 -0400

    functions notes

--- playground/types_functions.js
+++ playground/types_functions.js
@@ -6,3 +6,10 @@ console.log(boozy);
 
 let age2 = 100;
 let age3 = 100;
+
+// blah blah blah watched video...
+
+// some funtions return things to you, some just do work and don't return anything
+console.log('10'); // does a thing, doesn't return anything
+
+Date.now(); // returns unix time in milliseconds
